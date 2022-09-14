fd739eca811eb93bbbfedb7bbf4cfee764ed8c85
Michael Mannseicher <m.mansi1@gmx.net>
2022-09-15 00:00:39 +0200

    build: route to top if path change

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react"
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import { MainPage } from "./pages/Main"
-import { IMPRESS_ROUTE, MAIN_ROUTE, NOT_FOUND_ROUTE } from "./routes"
+import { BrowserRouter } from "react-router-dom"
 import de from "./assets/i18n/de.json"
 import en from "./assets/i18n/en.json"
 import { initI18next } from "./plugins/i18next"
 import { I18nextProvider, useTranslation } from "react-i18next"
 import { ThemeProvider } from "./theme/ThemeProvider"
-import { ImpressPage } from "./pages/Impress"
 import { getRouterBaseName } from "./functions/getRouterBaseName"
+import { AppRoutes } from "./route/AppRoutes"
+
 initI18next(
   {
     de,
@@ -24,14 +23,7 @@ function App() {
     <ThemeProvider>
       <I18nextProvider i18n={i18n}>
         <BrowserRouter basename={getRouterBaseName()}>
-          <Routes>
-            <Route path={MAIN_ROUTE} element={<MainPage />} />
-            <Route path={IMPRESS_ROUTE} element={<ImpressPage />} />
-            <Route
-              path={NOT_FOUND_ROUTE}
-              element={<Navigate to={MAIN_ROUTE} />}
-            />
-          </Routes>
+          <AppRoutes />
         </BrowserRouter>
       </I18nextProvider>
     </ThemeProvider>
---
+++ src/route/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import React, { useLayoutEffect } from "react"
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
+import { IMPRESS_ROUTE, MAIN_ROUTE, NOT_FOUND_ROUTE } from "../routes"
+import { MainPage } from "../pages/Main"
+import { ImpressPage } from "../pages/Impress"
+
+export const AppRoutes = () => {
+  const location = useLocation()
+  // Scroll to top if path changes
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+  return (
+    <Routes>
+      <Route path={MAIN_ROUTE} element={<MainPage />} />
+      <Route path={IMPRESS_ROUTE} element={<ImpressPage />} />
+      <Route path={NOT_FOUND_ROUTE} element={<Navigate to={MAIN_ROUTE} />} />
+    </Routes>
+  )
+}
