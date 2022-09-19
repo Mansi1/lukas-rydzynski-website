ce017c61e509be70d9d1a5806a2f5b0e83f585b5
Michael Mannseicher <m.mansi1@gmx.net>
2022-09-19 22:09:26 +0200

    build: add font

--- src/theme/themeOptions.ts
+++ src/theme/themeOptions.ts
@@ -1,7 +1,4 @@
-import "@fontsource/roboto/300.css"
-import "@fontsource/roboto/400.css"
-import "@fontsource/roboto/500.css"
-import "@fontsource/roboto/700.css"
+import "@fontsource/italiana"
 
 import { Theme, ThemeOptions } from "@mui/material/styles/createTheme"
 import { GlobalProps } from "@emotion/react"
@@ -14,7 +11,7 @@ export const themeOptions: ThemeOptions = {
     secondary: { main: "#ed8103" },
     background: { default: "#e3e8ff" },
   },
-  typography: { fontFamily: "Roboto" },
+  typography: { fontFamily: "Italiana" },
   components: {
     MuiButton: {
       defaultProps: {
@@ -31,6 +28,6 @@ export const getGlobalStyles = (theme: Theme): GlobalProps["styles"] => ({
     textDecoration: "none",
     cursor: "pointer",
   },
-  body: { fontFamily: "Roboto", margin: 0, padding: 0 },
+  body: { fontFamily: theme.typography.fontFamily, margin: 0, padding: 0 },
   html: { margin: 0, padding: 0 },
 })
