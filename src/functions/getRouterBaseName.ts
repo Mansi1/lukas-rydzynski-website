6837d7889de9eeb55a58b87425ac2d3c8b2bb50f
Michael Mannseicher <m.mansi1@gmx.net>
2022-10-15 20:11:49 +0200

    fix: delete get router base name

--- src/functions/getRouterBaseName.ts
+++
@@ -1,6 +0,0 @@
-import packagejson from "../../package.json"
-
-export const getRouterBaseName = () => {
-  const [, part] = packagejson.homepage.split("//")
-  return part.substring(part.indexOf("/"))
-}
